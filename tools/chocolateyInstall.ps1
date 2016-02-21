4128ed7154bd4acf4cde8ae10c5a1d88d2d69b45
Stefan Scherer <scherer_stefan@icloud.com>
2016-02-21 22:15:35 +0100

    Use Install-ChocolateyZipPackage again

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -23,15 +23,8 @@ if ([System.IO.Directory]::Exists("$env:ChocolateyInstall\lib\packer")) {
   }
 }
 
-# just to prepare the 0.9.0 package we use the AppVeyor artifcacts which are exe and not zip
-$file = "$($unzipLocation)\packer.exe"
-if ([System.IO.Directory]::Exists($unzipLocation)) {
-  [System.IO.Directory]::CreateDirectory($unzipLocation)
-}
-Get-ChocolateyWebFile $packageName $file $url $url64bit -checksum $checksum -checksumType $checksumType -checksum64 $checksum64 -checksumType64 $checksumType64
-
-#Install-ChocolateyZipPackage "packer" "$url" "$unzipLocation" "$url64bit" `
-# -checksum $checksum -checksumType 'sha1' -checksum64 $checksum64
+Install-ChocolateyZipPackage "packer" "$url" "$unzipLocation" "$url64bit" `
+ -checksum $checksum -checksumType 'sha1' -checksum64 $checksum64
 
 If (Test-Path $legacyLocation) {
   Write-Host "Removing old packer installation from $legacyLocation"
