ce258bd814a1b15ab9cdbb3ca57245643090cb68
Stefan Scherer <scherer_stefan@icloud.com>
2016-05-07 19:48:31 +0200

    Update packer 0.10.1

--- get-checksum.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo 32bit
-curl https://releases.hashicorp.com/packer/$1/packer_$1_windows_386.zip | shasum -a 1 -
-echo 64bit
-curl https://releases.hashicorp.com/packer/$1/otto_$1_windows_amd64.zip | shasum -a 1 -
