2edd63043ff1b037263fe95b519b4ea864a437c4
Johnsp123 <138159745+Johnsp123@users.noreply.github.com>
2024-05-13 19:26:06 -0700

    Revert "Update Packer 1.10.2"

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
-$url = 'https://releases.hashicorp.com/packer/1.10.2/packer_1.10.2_windows_386.zip'
-$checksum = '9aaf5f4caba87845377fabbc7dcd12d7add88dbdcaa0fd9f7427d201d7bf463c'
+$url = 'https://releases.hashicorp.com/packer/1.10.0/packer_1.10.0_windows_386.zip'
+$checksum = '5a62f84b50f35108f6fea4bae2cd2ca6e51ddedd00630baf64ea4d0c52cb811c'
 $checksumType = 'sha256'
-$url64 = 'https://releases.hashicorp.com/packer/1.10.2/packer_1.10.2_windows_amd64.zip'
-$checksum64 = 'acdbdb12e405f58f2d9b519c31d4ac5b2417f97bd7c2bfc93c95db03fc3fc1b1'
+$url64 = 'https://releases.hashicorp.com/packer/1.10.0/packer_1.10.0_windows_amd64.zip'
+$checksum64 = 'dc3171a5384062c195f8cd9c2dc25a7fcb35f4ddaefceb9e81b6f0b44307cda2'
 $checksumType64 = $checksumType
 $legacyLocation = "$env:SystemDrive\HashiCorp\packer"
 $unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
